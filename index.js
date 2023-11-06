cef9e6339d7c1bc01ef7adeea0af921ce4cd150a
Adam <adam5596@stud.kea.dk>
2023-11-06 15:02:37 +0100

    rest

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! + rasmus");
 });
 
 app.get("/endpoint", (req, res) => {
