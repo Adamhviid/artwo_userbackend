74ef44b5cfb3eb7f532a682d619fcab36b10c6b6
Adam <adam5596@stud.kea.dk>
2023-11-06 11:44:56 +0100

    added azure

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${process.env.PORT || 8080}...`);
+});
+
+
