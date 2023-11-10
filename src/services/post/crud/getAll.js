d21382fb8204fa3db23f560de47faf890f4fbed8
Adam Kanstrup Hviid <akh@pairy.dk>
2023-11-10 13:35:09 +0100

    pagination

--- src/services/post/crud/getAll.js
+++ src/services/post/crud/getAll.js
@@ -1,22 +1,26 @@
 import postModel from "../../../models/post.js";
 import likeModel from "../../../models/like.js";
-import commentModel from "../../../models/comment.js"
+import commentModel from "../../../models/comment.js";
 
 export default async function getAll(req, res) {
     try {
+        const { page, pageSize } = req.query;
+        const offset = (page - 1) * pageSize;
+        const postPages = await postModel.count();
+
         const posts = await postModel.findAll({
-            order: [
-                ['updatedAt', 'DESC']
-            ]
-        });
-        const likes = await likeModel.findAll();
-        const comments = await commentModel.findAll({
-            order: [
-                ['updatedAt', 'DESC']
-            ]
+            order: [['updatedAt', 'DESC']],
+            limit: parseInt(pageSize),
+            offset: parseInt(offset),
         });
 
-        res.status(200).json({ posts, likes, comments })
+        // Fetch likes and comments without pagination for simplicity
+        const likes = await likeModel.findAll();
+        const comments = await commentModel.findAll({
+            order: [['updatedAt', 'DESC']],
+        });
+
+        res.status(200).json({ posts, postPages, likes, comments });
 
     } catch (err) {
         res.status(500).json(err);
